d4d7166f02126b97a70640e683542b7030319496
SwayZhao <zhswee@gmail.com>
2020-06-18 17:43:09 +0800

    scripts for installation automatically

--- SudoDEM2D/pkg/dem/Superellipse.cpp
+++ SudoDEM2D/pkg/dem/Superellipse.cpp
@@ -210,14 +210,14 @@ void Bo1_Superellipse_Aabb::go(const shared_ptr<Shape>& ig, shared_ptr<Bound>& b
 	Vector2r halfsize(r_max,r_max);
 	//the aabb should be optimized in future.
 	if(!scene->isPeriodic){
-		aabb->min=se2.position-halfSize; aabb->max=se2.position+halfSize;
+		aabb->min=se2.position-halfsize; aabb->max=se2.position+halfsize;
 		return;
 	}
 	if(scene->cell->hasShear()) {//debug for 2d
 	Vector2r refHalfSize(halfsize);
 	const Real& _cos=scene->cell->getCos();
 		//cerr<<"cos["<<i<<"]"<<cos[i]<<" ";
-		halfSize+=.5*refHalfSize*(1/_cos-1);
+		halfsize+=.5*refHalfSize*(1/_cos-1);
 	}
 	//cerr<<" || "<<halfSize<<endl;
 	aabb->min = scene->cell->unshearPt(se2.position)-halfsize;
---
+++ scripts/firstCompile.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+WORKSPACE=$PWD
+#6. Compile SudoDEM2D
+mkdir build2d
+cd build2d
+cmake ../SudoDEM2D
+make
+#make -j3 #use 3 threads to compile
+make install
+#the compiled files will be install at `sudodeminstall/SudoDEM2D/'.
+#copy the dynamic files `3rdlibs' into `sudodeminstall/SudoDEM2D/lib/'
+cd $WORKSPACE
+cp -rf 3rdlibs sudodeminstall/SudoDEM2D/lib/
+#make sure to copy IPython pakage into `sudodeminstall/SudoDEM2D/lib/py'.
+
+#7. Set PATH in ~/.bashrc
+#Append the following command into `~/.bashrc'.
+#PATH=${PATH}:/home/xxx/SudoDEM/sudodeminstall/SudoDEM2D/bin
+echo 'PATH=${PATH}:/home/xxx/SudoDEM/sudodeminstall/SudoDEM2D/bin'>> ~/.bashrc
