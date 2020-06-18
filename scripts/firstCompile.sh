a3297776293b9f70ae3c2e3534d769a3cd520657
SwayZhao <zhswee@gmail.com>
2020-06-18 23:07:19 +0800

    updated fistCompile.sh

--- scripts/firstCompile.sh
+++ scripts/firstCompile.sh
@@ -11,7 +11,7 @@ make install
 #the compiled files will be install at `sudodeminstall/SudoDEM2D/'.
 #copy the dynamic files `3rdlibs' into `sudodeminstall/SudoDEM2D/lib/'
 cd $WORKSPACE
-cp -rf 3rdlibs sudodeminstall/SudoDEM2D/lib/
+cp -rf 3rdlib/3rdlibs sudodeminstall/SudoDEM2D/lib/
 #make sure to copy IPython pakage into `sudodeminstall/SudoDEM2D/lib/py'.
 
 #7. Set PATH in ~/.bashrc
