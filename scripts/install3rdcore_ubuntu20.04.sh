e7e90729dba882e6f517142e0360de8225e12d0a
SwayZhao <zhswee@gmail.com>
2023-11-08 19:59:48 +0800

    add ci and scripts

---
+++ scripts/install3rdcore_ubuntu20.04.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+OS="ubuntu-20.04"
+
+cd ..
+mkdir -p 3rdlib/3rdlibs/py
+
+# download pre-compiled tools
+wget https://github.com/SudoDEM/3rdlibs/releases/latest/download/tools-${OS}.tar.xz
+tar xf tools-${OS}.tar.xz
+
+cd 3rdlib
+# download pre-compiled libs
+wget https://github.com/SudoDEM/3rdlibs/releases/latest/download/3rdlibs-${OS}.tar.xz
+tar xf 3rdlibs-${OS}.tar.xz
+
+
+#cp all libraries
+cp -rf HeaderLib/lib/* 3rdlibs/
+cp -r HeaderLib/lib/py/* 3rdlibs/py/
