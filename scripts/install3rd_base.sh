66bb4ac45c31718a56d87d55c1c9489bb9375ceb
Sway <zhswee@gmail.com>
2024-12-12 18:19:23 +0800

    Update install3rd_base.sh

--- scripts/install3rd_base.sh
+++ scripts/install3rd_base.sh
@@ -4,8 +4,8 @@
 sudo apt-get install -y build-essential cmake zlib1g-dev freeglut3-dev libbz2-dev libglib2.0 python3-dev python3-pip python3-tk
 
 # Qt5 based library
-sudo apt-get install -y qt5-default libqt5svg5-dev libqt5webkit5-dev libqt5designer5
-
+#sudo apt-get install -y qt5-default libqt5svg5-dev libqt5webkit5-dev libqt5designer5
+sudo apt-get install -y qtbase5-dev qt5-qmake libqt5svg5-dev libqt5webkit5-dev libqt5designer5
 # python pip dependencies
 python3 -m pip install numpy matplotlib xlib ipython
 
